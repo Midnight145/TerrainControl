836f7e9cd60497f28e727ff2aa458e43f48e9936
Midnight <23142346+Midnight145@users.noreply.github.com>
2023-02-14 01:09:43 -0600

    send configs iff on dedicated server

--- platforms/forge/src/main/java/com/khorn/terraincontrol/forge/events/PlayerTracker.java
+++ platforms/forge/src/main/java/com/khorn/terraincontrol/forge/events/PlayerTracker.java
@@ -17,41 +17,50 @@ import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.network.play.server.S3FPacketCustomPayload;
 
 public class PlayerTracker {
-
+	
 	@SubscribeEvent
 	public void onPlayerLogin(PlayerEvent.PlayerLoggedInEvent event) {
 		// Server-side - called whenever a player logs in
 		// I couldn't find a way to detect if the client has TerrainControl,
 		// so for now the configs are sent anyway.
-
+		
 		// Get the config
 		if (!(event.player instanceof EntityPlayerMP)) { return; }
-
-		EntityPlayerMP player = (EntityPlayerMP) event.player;
-
-		LocalWorld worldTC = WorldHelper.toLocalWorld(player.getEntityWorld());
+		
+		final EntityPlayerMP player = (EntityPlayerMP) event.player;
+		
+		final LocalWorld worldTC = WorldHelper.toLocalWorld(player.getEntityWorld());
 		if (worldTC == null) {
 			// World not loaded
 			return;
 		}
-		ConfigProvider configs = worldTC.getConfigs();
-
+		final ConfigProvider configs = worldTC.getConfigs();
+		
 		// Serialize it
-		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-		DataOutputStream stream = new DataOutputStream(outputStream);
+		final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+		final DataOutputStream stream = new DataOutputStream(outputStream);
 		try {
 			stream.writeInt(PluginStandardValues.ProtocolVersion);
 			configs.writeToStream(stream);
-		} catch (IOException e) {
+		}
+		catch (final IOException e) {
 			TerrainControl.printStackTrace(LogMarker.FATAL, e);
 		}
-
+		
 		// Make the packet
-		S3FPacketCustomPayload packet = new S3FPacketCustomPayload(PluginStandardValues.ChannelName, outputStream.toByteArray());
-
-		// Send the packet
-//		player.playerNetServerHandler.sendPacket(packet);
+		final S3FPacketCustomPayload packet = new S3FPacketCustomPayload(PluginStandardValues.ChannelName,
+				outputStream.toByteArray());
+		
+		try {
+			Class.forName("net.minecraft.client.Minecraft");
+		}
+		catch (final ClassNotFoundException exception) {
+			System.out.println(
+					"net.minecraft.client.Minecraft doesn't exist, assuming dedicated server. Sending configs");
+			// Send the packet
+			player.playerNetServerHandler.sendPacket(packet);
+		}
 
 	}
-
+	
 }
